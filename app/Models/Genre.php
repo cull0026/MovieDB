0f1d11a97dfd0ec276a5b1300d96b16341e06543
Pat <cull0026@algonquinlive.com>
2026-07-21 15:16:49 -0400

    Hadn't done a commit yet and should probably have a backup, no?

--- app/Models/Genre.php
+++ app/Models/Genre.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Genre extends Model
 {
-    //
+    protected $fillable = ['name'];
+    public function movies(){
+        return $this->belongsToMany(Movie::class);
+    }
 }
--- app/Models/Movie.php
+++ app/Models/Movie.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Movie extends Model
 {
-    //
+    protected $fillable = ['title', 'release_year'];
+    public function genres(){
+        return $this->belongsToMany(Genre::class);
+    }
 }
